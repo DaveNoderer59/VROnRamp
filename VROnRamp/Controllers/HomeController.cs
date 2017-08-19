338ec930b9516dfbb6df37b925afede4876803d9
Dave Noderer <daven@computerways.com>
2017-08-19 14:10:56 -0400

    Initial Checking

--- VROnRamp/Controllers/HomeController.cs
+++ VROnRamp/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using VROnRamp.Models;
 
 namespace VROnRamp.Controllers
 {
@@ -31,7 +29,7 @@ namespace VROnRamp.Controllers
 
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
