4fc2fdb50d3f4df68980f4e08dd07106d8cf54db
Dave Noderer <daven@computerways.com>
2017-08-19 14:03:49 -0400

    Add project files.

---
+++ VROnRamp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VROnRamp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
